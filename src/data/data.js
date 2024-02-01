d431c9dec64df1513eea591ab18dfb54bab5eae2
Victor Raul <v1c70rotakmer@gmail.com>
2024-02-01 12:44:20 -0500

    Fix: update images for the portfolio and config

--- src/data/data.js
+++ src/data/data.js
@@ -1,39 +1,39 @@
 export const projectsData = [
-  {
-    id: 1,
-    title: "StreamView",
-    date: "2021",
-    languages: ["React", "Tailwind CSS", "NodeJS", "Express", "MongoDB"],
-    description:
-      "Aplicación que facilita al usuario saber en que plataformas de streaming se encuentra disponible una película o serie.",
-    img: "https://media.licdn.com/dms/image/D4E2DAQFMYtGLlYA4yQ/profile-treasury-image-shrink_800_800/0/1703304721737?e=1707080400&v=beta&t=TavH3imIQ3YFwavK2AOKxcpmck1VtCMdRd1scvHgS2A",
-    link: "https://www.google.com",
-  },
-  {
-    id: 2,
-    title: "Billeta Virtual",
-    date: "2021",
-    languages: [
-      "React",
-      "Recharts",
-      "Tailwind CSS",
-      "NodeJS",
-      "Express",
-      "MongoDB",
-    ],
-    description:
-      "Billetera Virtual para la transacción de dinero entre usuarios. Brindando un dashboard para el control de las transacciones y el saldo de la cuenta. Desarrollada po a un equipo de 3 personas en una emulación laboral de No Country",
-    img: "https://media.licdn.com/dms/image/D4E2DAQF03bg-NCYooA/profile-treasury-image-shrink_800_800/0/1703304893542?e=1707080400&v=beta&t=sj0vhLfUFZjjrR3eZ99mXcsoYBqAcYqTDCBqgWUj9lw",
-    link: "https://anderson.d301togxhux3st.amplifyapp.com/",
-  },
-  {
-    id: 3,
-    title: "Atack Heart",
-    date: "2021",
-    languages: ["Astro", "Tailwind CSS", "FastAPI", "Python"],
-    description:
-      "Aplicacion web que permite al usuario conocer su riesgo de sufrir un ataque al corazón usando un algoritmo de machine learning.",
-    img: import("../assets/heart-attack.png"),
-    link: "https://github.com/valec3/Heart-Attack",
-  },
+    {
+        id: 1,
+        title: 'Billeta Virtual',
+        date: '2021',
+        languages: [
+            'React',
+            'Recharts',
+            'Tailwind CSS',
+            'NodeJS',
+            'Express',
+            'MongoDB',
+        ],
+        description:
+            'Billetera Virtual para la transacción de dinero entre usuarios. Brindando un dashboard para el control de las transacciones y el saldo de la cuenta. Desarrollada po a un equipo de 3 personas en una emulación laboral de No Country',
+        img: import('../assets/WarrenBank.jpg'),
+        link: 'https://anderson.d301togxhux3st.amplifyapp.com/',
+    },
+    {
+        id: 2,
+        title: 'StreamView',
+        date: '2021',
+        languages: ['React', 'Tailwind CSS', 'NodeJS', 'Express', 'MongoDB'],
+        description:
+            'Aplicación que facilita al usuario saber en que plataformas de streaming se encuentra disponible una película o serie.',
+        img: import('../assets/StreamView.jpg'),
+        link: 'https://s12-07-n-node-react.vercel.app/',
+    },
+    {
+        id: 3,
+        title: 'Atack Heart',
+        date: '2021',
+        languages: ['Astro', 'Tailwind CSS', 'FastAPI', 'Python'],
+        description:
+            'Aplicacion web que permite al usuario conocer su riesgo de sufrir un ataque al corazón usando un algoritmo de machine learning.',
+        img: import('../assets/heart-attack.png'),
+        link: 'https://heart-attack-steel.vercel.app/',
+    },
 ];
